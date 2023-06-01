2fa0847fca18767cc77a82f695411866682d440f
Emmanuel Sanchez <fede@fede.website>
2023-06-01 16:49:52 -0600

    added javascript to the connection of the page

--- client/main.js
+++ client/main.js
@@ -1,1 +1,32 @@
+function createSocket(){
+  //Action Cable has a Websocket mounted at;
+  // ws://localhost:3000/cable
+  const socket_url = 'ws://localhost:3000/cable';
+  const socket = new WebSocket(socket_url);
 
+  // When the sockect is opened, we can send data to the server
+  socket.onopen = function(event) {
+    console.log('Connected to the server');
+    const msg = {
+      command: 'suscribe',
+      identifier: JSON.stringify({
+        id: 1,
+        channel: 'AlertsChannel',
+      })
+    };
+    socket.send(JSON.stringify(msg));
+  }
+
+  socket.onmessage = function(event) {
+    console.log("Received data from the server", event.data);
+  }
+
+  socket.onclose = function(event) {
+    console.log('Diesconnected from the server');
+  }
+  socket.onerror = function(error) {
+    console.log('Websocket error observed:', error);
+  }
+}
+
+createSocket();
